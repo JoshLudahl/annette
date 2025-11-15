90a75c4130764f59e76d5c35b00c4f148f98c9c7
joshy <josh.ludahl@gmail.com>
2025-11-15 14:45:08 -0800

    Refactor: Remove unused Dp import
    This commit removes an unused import of `androidx.compose.ui.unit.Dp` from `CommonUI.kt`.

--- core/src/main/java/com/softklass/annette/core/ui/CommonUI.kt
+++ core/src/main/java/com/softklass/annette/core/ui/CommonUI.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlin.math.max
 
